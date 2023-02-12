29eb8f2b3ac65f0441e3cbf1f4f4234cd1402330
anitajahiu <77509129+anitajahiu@users.noreply.github.com>
2023-02-12 09:53:27 -0800

    Add files via upload

---
+++ models/products.php
@@ -0,0 +1,37 @@
+<?php
+
+    $productIndex = array(
+        ["src"=> "/Web Project/img/r1.jpg", "name"=> "Cut Out Sugar Cookie Recipe", "link"=>"christmas.php"],
+        ["src"=> "/Web Project/img/r2.jpg", "name"=> "M&M Cookies Recipe", "link"=>"christmas.php"],
+        ["src"=> "/Web Project/img/r3.jpg", "name"=> "Tiramisu", "link"=>"desserts.php"],
+        ["src"=> "/Web Project/img/r4.jpg", "name"=> "Cream of Mushroom Soup", "link"=>"mainDish.php"],
+        ["src"=> "/Web Project/img/r5.jpg", "name"=> "Easy Pasta Salad", "link"=>"appetizers.php"],
+        ["src"=> "/Web Project/img/r6.jpg", "name"=> "Marinated Grilled Salmon", "link"=>"mainDish.php"]
+    );
+
+
+
+    $productRecipes = array(
+        ["src"=> "/Web Project/img/llr1.jpg", "name"=> "Appetizers & Salads", "link"=>"appetizers.php"],
+        ["src"=> "/Web Project/img/llr2.jpg", "name"=> "Breakfast", "link"=>"breakfast.php"],
+        ["src"=> "/Web Project/img/llr3.jpg", "name"=> "Main Dish", "link"=>"mainDish.php"],
+        ["src"=> "/Web Project/img/llr4.jpg", "name"=> "Christmas", "link"=>"christmas.php"],
+        ["src"=> "/Web Project/img/llr5.jpg", "name"=> "Drinks", "link"=>"drinks.php"],
+        ["src"=> "/Web Project/img/llr6.jpg", "name"=> "Desserts", "link"=>"desserts.php"]
+    );
+
+
+    $aboutUs = array(
+        ["title"=>"Our Story", "description"=>"Recipes is a place for me to share the food I love as I hope to expand my photography and writing skills.
+            I love all things food. Let me rephrase that, I love all things good food. I’m picky about what I like and 
+            I’d like to say I know great food. My favorite foods include all things Italian or Mexican, anything with chocolate, 
+            caramel, fresh fruit or browned butter (is there a better scent in the world?). I don’t know how I’d live without the 
+            comfort food classics like pizza and enchiladas, all those indulgent dishes loaded with an abundance of gooey melted cheese. 
+            I’m a meat eater. Cookie dough is my weakness. The one food I think should be illegal is canned beets, they terrify me. 
+            Sugar is my addiction. I could live in the kitchen. I like to make food look pretty, because when it looks 
+            good doesn’t it just taste better?", "description2"=>"My goal here is to publish recipes that you, your family and your friends will not only love but will want to make time 
+            and time again. I hope to inspire you as a home cook and boost your confidence in the kitchen. Life is meant to be enjoyed 
+            and doesn’t good food just make it more enjoyable?", "p1"=>"+383 38 541 400", "p2"=>"recipes@gmail.com", "p3"=>"Lagjja Kalabria,10000 Prishtine, Kosovo" ]
+    );
+
+?>
