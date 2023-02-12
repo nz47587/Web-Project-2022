e73988e14f10bf9d81f5f14493c4dc916ab74459
anitajahiu <77509129+anitajahiu@users.noreply.github.com>
2023-02-12 10:04:54 -0800

    Add files via upload

---
+++ view/footer.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Footer</title>
+    <style>
+        *{
+            padding: 0;
+            margin: 0;
+        }
+        footer div{
+            height: 60px;
+            width: 100%;
+            background-color:#f2f2f2;
+        }
+
+        footer div p{
+            text-align: center;
+            font-size: larger;
+            padding-top: 20px;
+        }
+        
+
+    </style>
+</head>
+<body>
+    
+    <footer>
+        <div>
+            <p>©2022 Recipes</p>
+        </div>
+    </footer>
+    
+</body>
+</html>
